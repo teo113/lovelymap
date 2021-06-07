8c0e56adfbbc8b4727ffa6e5429b32437c9b8784
teo113 <faull.theo@gmail.com>
2021-06-07 13:57:51 +0100

    added basic 27700 ol, and empty leaflet

---
+++ leaflet/static/scripts/leaflet-map.js
@@ -0,0 +1,13 @@
+// Setup the EPSG:27700 (British National Grid) projection.
+var crs = new L.Proj.CRS('EPSG:27700', '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs', {
+    resolutions: [ 896.0, 448.0, 224.0, 112.0, 56.0, 28.0, 14.0, 7.0, 3.5, 1.75 ]
+});
+
+
+var map = L.map('leaflet-map', {crs: crs}).setView([51.505, -0.09], 5);
+
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Map data',
+    maxZoom: 18,
+    id: 'arcgis'
+}).addTo(map);
--- openlayers/static/scripts/ol-map.js
+++ openlayers/static/scripts/ol-map.js
@@ -1,24 +1,41 @@
 ////////////////////////////// MAP SETTINGS
-proj4.defs("EPSG:27700","+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.060,0.1502,0.2470,0.8421,-20.4894 +datum=OSGB36 +units=m +no_defs");
+proj4.defs(
+  'EPSG:27700',
+  '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 ' +
+    '+x_0=400000 +y_0=-100000 +ellps=airy ' +
+    '+towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 ' +
+    '+units=m +no_defs'
+);
 ol.proj.proj4.register(proj4);
-var mapproj = ol.proj.get('EPSG:27700');
+var proj27700 = ol.proj.get('EPSG:27700');
+var mapcenter = [310000, 411000]
 var mapextent = [-238375, 0, 908505, 1376256];
-var mapcenter = [356816, 172539]
+
+////////////////////////////// MAP CONTROLS
+var mousePositionCtrl = new ol.control.MousePosition({
+  coordinateFormat: ol.coordinate.createStringXY(2),
+  projection: proj27700,
+  target: document.getElementById('locationText'),
+  undefinedHTML: '&nbsp;'
+});
 
 ////////////////////////////// MAP OBJECT
+var view = new ol.View({
+    projection: proj27700,
+    center: mapcenter,
+    extent: mapextent,
+    zoom: 6,
+    minZoom: 6,
+    maxZoom: 10
+});
+
 var map = new ol.Map({
-    target: 'map',
+    target: 'ol-map',
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM()
       })
     ],
-    view: new ol.View({
-      projection: mapproj,
-      center: mapcenter,
-      extent: mapextent,
-      zoom: 5,
-      minZoom: 4,
-      maxZoom: 18
-    })
-  });
+    controls: ol.control.defaults().extend([mousePositionCtrl]),
+    view: view
+    });
