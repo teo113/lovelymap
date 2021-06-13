d06e806ae31ec08e95249f70e8fe28d6655e4975
teo113 <faull.theo@gmail.com>
2021-06-13 23:53:49 +0100

    ol and leaf updates

--- leaflet/static/scripts/leaflet-map.js
+++ leaflet/static/scripts/leaflet-map.js
@@ -1,13 +1,51 @@
-// Setup the EPSG:27700 (British National Grid) projection.
+////////////////////////////// PROJECTION SETTINGS
 var crs = new L.Proj.CRS('EPSG:27700', '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs', {
-    resolutions: [ 896.0, 448.0, 224.0, 112.0, 56.0, 28.0, 14.0, 7.0, 3.5, 1.75 ]
+    resolutions: [ 896.0, 448.0, 224.0, 112.0, 56.0, 28.0, 14.0, 7.0, 3.5, 1.75 ],
+    origin: [ -238375.0, 1376256.0 ]
 });
 
+////////////////////////////// COORDINATE TRANSFORM FUNCTION
+var transformCoords = function(arr) {
+    return proj4('EPSG:27700', 'EPSG:4326', arr).reverse();
+};
 
-var map = L.map('leaflet-map', {crs: crs}).setView([51.505, -0.09], 5);
+////////////////////////////// MAP LAYERS
+os_key = 'N3vMFK52P63Ao1dAp4cRq31OHpnXz5rW'
 
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Map data',
-    maxZoom: 18,
-    id: 'arcgis'
-}).addTo(map);
+var os_outdoor = L.tileLayer('https://api.os.uk/maps/raster/v1/zxy/Outdoor_27700/{z}/{x}/{y}.png?key=' + os_key)//.addTo(map);
+var os_light = L.tileLayer('https://api.os.uk/maps/raster/v1/zxy/Light_27700/{z}/{x}/{y}.png?key=' + os_key)//.addTo(map);
+
+var arcgis_topo = L.esri.tiledMapLayer({
+    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer',
+    maxZoom: 19,
+    minZoom: 6
+    });//.addTo(map);
+
+////////////////////////////// MAP SETTINGS
+var map_options = {
+    layers: [os_outdoor],
+    crs: crs,
+    minZoom: 0,
+    maxZoom: 9,
+    center: transformCoords([ 337297, 503695 ]),
+    zoom: 7,
+    maxBounds: [
+        transformCoords([ -238375.0, 0.0 ]),
+        transformCoords([ 900000.0, 1376256.0 ])
+    ],
+    attributionControl: false
+};
+
+////////////////////////////// MAP OBJECT
+//var map = L.map('leaflet-map', {crs: crs, attributionControl: false}).setView([51.505, -0.09], 7);
+var map = L.map('leaflet-map', map_options);
+
+////////////////////////////// MAP CONTROLS
+var basemaps = {
+    'Light': os_light,
+    'Outdoor': os_outdoor
+};
+
+var overlaymaps = {};
+
+L.control.layers(basemaps, overlaymaps).addTo(map);
--- openlayers/static/scripts/ol-map.js
+++ openlayers/static/scripts/ol-map.js
@@ -8,8 +8,61 @@ proj4.defs(
 );
 ol.proj.proj4.register(proj4);
 var proj27700 = ol.proj.get('EPSG:27700');
-var mapcenter = [310000, 411000]
+var mapcenter = [310000, 511000]
 var mapextent = [-238375, 0, 908505, 1376256];
+
+var tilegrid = new ol.tilegrid.TileGrid({
+  resolutions: [ 896.0, 448.0, 224.0, 112.0, 56.0, 28.0, 14.0, 7.0, 3.5, 1.75 ],
+  origin: [ -238375.0, 1376256.0 ]
+});
+
+////////////////////////////// MAP LAYERS
+os_service = 'https://api.os.uk/maps/raster/v1/zxy'
+os_key = 'N3vMFK52P63Ao1dAp4cRq31OHpnXz5rW'
+
+var basemaps = new ol.layer.Group({
+  'title': 'Basemaps',
+  layers: [
+      new ol.layer.Tile({
+          title: 'OSM',
+          type: 'base',
+          source: new ol.source.OSM()
+     }),
+      new ol.layer.Tile({
+          title: 'Road',
+          type: 'base',
+          visible: false,
+          source: new ol.source.XYZ({
+              url: os_service + '/Road_27700/{z}/{x}/{y}.png?key=' + os_key,
+              projection: 'EPSG:27700',
+              tileGrid: tilegrid,
+              attributions: 'Contains OS data © Crown copyright and database right 2021'
+          })
+      }),
+      new ol.layer.Tile({
+          title: 'Outdoor',
+          type: 'base',
+          visible: false,
+          source: new ol.source.XYZ({
+              url: os_service + '/Outdoor_27700/{z}/{x}/{y}.png?key=' + os_key,
+              projection: 'EPSG:27700',
+              tileGrid: tilegrid,
+              attributions: 'Contains OS data © Crown copyright and database right 2021'
+          })
+      }),
+      new ol.layer.Tile({
+          title: 'Light',
+          type: 'base',
+          visible: false,
+          source: new ol.source.XYZ({
+              url: os_service + '/Light_27700/{z}/{x}/{y}.png?key=' + os_key,
+              projection: 'EPSG:27700',
+              tileGrid: tilegrid,
+              attributions: 'Contains OS data © Crown copyright and database right 2021'
+          })
+      })
+  ]
+});
 
 ////////////////////////////// MAP CONTROLS
 var mousePositionCtrl = new ol.control.MousePosition({
@@ -19,12 +72,14 @@ var mousePositionCtrl = new ol.control.MousePosition({
   undefinedHTML: '&nbsp;'
 });
 
+var layerSwitcher = new ol.control.LayerSwitcher();
+
 ////////////////////////////// MAP OBJECT
 var view = new ol.View({
     projection: proj27700,
     center: mapcenter,
     extent: mapextent,
-    zoom: 6,
+    zoom: 8,
     minZoom: 6,
     maxZoom: 10
 });
@@ -32,10 +87,8 @@ var view = new ol.View({
 var map = new ol.Map({
     target: 'ol-map',
     layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
+      basemaps
     ],
-    controls: ol.control.defaults().extend([mousePositionCtrl]),
+    controls: ol.control.defaults().extend([mousePositionCtrl, layerSwitcher]),
     view: view
     });
